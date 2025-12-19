2179d03ec7d9f644f1afad88cecead73b0bfc251
Sufyan Liaquat <sufyanliaquat58@gmail.com>
2025-12-19 23:49:49 +0500

    updated docker-compose and prometheus config

--- index.js
+++ index.js
@@ -14,4 +14,37 @@ const logger = createLogger({
 
 const collectDefaultMetrics = promClient.collectDefaultMetrics;
 
-collectDefaultMetrics
+collectDefaultMetrics();
+
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+    name: 'http_request_duration_ms',
+    help: 'Duration of HTTP requests in ms',
+    labelNames: ['method', 'route', 'code'],
+    buckets: [50, 100, 200, 300, 400, 500, 750, 1000]
+});
+
+const httpRequestsTotal = new promClient.Counter({
+    name: 'http_requests_total',
+    help: 'Total number of HTTP requests',
+});
+
+app.use(responseTime((req, res, time) => {
+    httpRequestsTotal.inc();
+    httpRequestDurationMicroseconds.labels(req.method, req.path, res.statusCode).observe(time);
+}));
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+    logger.info("Root endpoint was hit");
+});
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', promClient.register.contentType);
+    res.end(await promClient.register.metrics());
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+}); 
+
